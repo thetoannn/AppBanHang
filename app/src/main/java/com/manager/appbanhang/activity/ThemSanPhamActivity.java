665a0c1673f576f1106d109017dcc8390e0b50b1
thetoannn <naotehtgnoud@gmail.com>
2024-07-31 03:32:22 +0700

    fixAddSanPham

--- app/src/main/java/com/manager/appbanhang/activity/ThemSanPhamActivity.java
+++ app/src/main/java/com/manager/appbanhang/activity/ThemSanPhamActivity.java
@@ -1,10 +1,12 @@
 package com.manager.appbanhang.activity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,20 +16,32 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.manager.appbanhang.R;
 import com.manager.appbanhang.databinding.ActivityThemSanPhamBinding;
+import com.manager.appbanhang.retrofit.ApiBanHang;
+import com.manager.appbanhang.retrofit.RetrofitClient;
+import com.manager.appbanhang.utils.Utils;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class ThemSanPhamActivity extends AppCompatActivity {
     Spinner spinner;
     int loai = 0;
     ActivityThemSanPhamBinding binding;
+    ApiBanHang apiBanHang;
+    CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_them_san_pham);
+        binding = ActivityThemSanPhamBinding.inflate(getLayoutInflater());
+        apiBanHang = RetrofitClient.getInstance(Utils.BASE_URL).create(ApiBanHang.class);
+        setContentView(binding.getRoot());
+
         initView();
         initData();
     }
@@ -39,15 +53,58 @@ public class ThemSanPhamActivity extends AppCompatActivity {
         stringList.add("Loại 2");
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, stringList);
         spinner.setAdapter(adapter);
-        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 loai = i;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        binding.btnThem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                themsanpham();
             }
         });
     }
 
+    private void themsanpham() {
+        String str_ten = binding.tensp.getText().toString().trim();
+        String str_gia = binding.giasp.getText().toString().trim();
+        String str_mota = binding.mota.getText().toString().trim();
+        String str_hinhanh = binding.hinhanh.getText().toString().trim();
+        if (TextUtils.isEmpty(str_ten) || TextUtils.isEmpty(str_gia) || TextUtils.isEmpty(str_mota) || TextUtils.isEmpty(str_hinhanh) || loai == 0) {
+            Toast.makeText(getApplicationContext(), "Vui lòng nhập đủ thông tin", Toast.LENGTH_SHORT).show();
+        } else {
+            compositeDisposable.add(apiBanHang.insertSp(str_ten, str_gia, str_hinhanh, str_mota, (loai))
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(
+                            messageModel -> {
+                                if (messageModel.isSuccess()) {
+                                    Toast.makeText(getApplicationContext(), messageModel.getMessage(), Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(getApplicationContext(), messageModel.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            },
+                            throwable -> {
+                                Toast.makeText(getApplicationContext(), throwable.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                    ));
+        }
+    }
+
     private void initView() {
         spinner = findViewById(R.id.spinner_loai);
+    }
+
+    @Override
+    protected void onDestroy() {
+        compositeDisposable.clear();
+        super.onDestroy();
     }
 }
